f77a638151ad30886587f0f8c35cab7170964a21
sandrasoueid <soueidsandra@gmail.com>
2016-04-20 08:01:23 -0700

    first commit

---
+++ app/src/androidTest/java/com/android/lab3/tests/JUnit_test.java
@@ -0,0 +1,27 @@
+package com.android.lab3.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.android.lab3.MainActivity;
+import com.android.lab3.R;
+
+/**
+ * Created by Sandra Soueid on 4/20/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+    public JUnit_test(){
+        super(MainActivity.class);
+
+    }
+
+    public void test_first(){
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Bye World", tester);
+    }
+}
